92f9763104a16f78f247d3c9e907bdbf91ac9f5b
morethanmin <mini4614@gmail.com>
2021-12-30 09:34:36 +0900

    [Spring Basic 1] 시작하기

--- [Spring Basic 1] 시작하기/src/main/java/com/jade/studyspring/StudySpringApplication.java
+++ [Spring Basic 1] 시작하기/src/main/java/com/jade/studyspring/StudySpringApplication.java
@@ -1,15 +1,23 @@
 package com.jade.studyspring;
+//해당 java파일을 패키지로 정의한다.
 
+//spring boot 패키지들을 가져온다.
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//가져온 클래스를 정의한다.
 @SpringBootApplication
+@RestController
 public class StudySpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudySpringApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 }
